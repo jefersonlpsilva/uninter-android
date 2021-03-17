cdbe847e6b9216edc09736d74b223b10bbeafc68
jefersonlpsilva <jefersonsilvadev@hotmail.com>
2021-03-17 04:14:13 -0300

    envio de argumento/parametro entre activities

--- ActivityResult/app/src/main/java/app/dinamismo/activityresult/MainActivity.java
+++ ActivityResult/app/src/main/java/app/dinamismo/activityresult/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView resultado;
     Button btnOpcao;
+    Button btnArgumento;
     static int ACAO_BUSCA_PREFERENCIA_USER = 1;
 
     @Override
@@ -19,12 +20,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //ligamos o componente btnOpcao a representacao no arquivo xml
         btnOpcao =(Button)findViewById(R.id.btnOpcao);
+        //ligamos o componente btnArgumento a representacao no arquivo xml
+        btnArgumento = (Button)findViewById(R.id.btnArgumento);
         //redefinir o listener OnClickListener
         btnOpcao.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(view.getContext(), SegundaActivity.class);
                 startActivityForResult(intent, ACAO_BUSCA_PREFERENCIA_USER);
+            }
+        });
+
+        btnArgumento.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(view.getContext(), TerceiraActivity.class);
+                intent.putExtra("retorno","Seu argumento chegou!!!");
+                startActivity(intent);
             }
         });
     }
---
+++ ActivityResult/app/src/main/java/app/dinamismo/activityresult/TerceiraActivity.java
@@ -0,0 +1,23 @@
+package app.dinamismo.activityresult;
+
+import android.content.Intent;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TerceiraActivity extends AppCompatActivity {
+
+    TextView valorParametro ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terceira);
+
+        valorParametro = (TextView)findViewById(R.id.valorParametro);
+
+        Intent intent = getIntent();
+        if ((intent.hasExtra("retorno")) && ( null != intent.getStringExtra("retorno")))
+          valorParametro.setText(intent.getStringExtra("retorno"));
+    }
+}
